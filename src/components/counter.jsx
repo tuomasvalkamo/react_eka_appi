1d1185b814c96b555acc3155d65d9563a0182137
Tuomas Valkamo <you@example.com>
2020-03-12 23:20:51 +0200

    Counter.jsx - in progress

---
+++ src/components/counter.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  handleIncrement() {
+    console.log("Increment clicked!", this);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btm-sm"
+        >
+          Increment
+        </button>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
